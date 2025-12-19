1639e1f937332b432bde2ac2b5645d8240422405
bala-amypo <bala@amypo.in>
2025-12-19 04:07:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -15,5 +15,45 @@ public class University {
     private String country;
     private Boolean active = true;
 
-    
+    public University() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccreditationLevel() {
+        return accreditationLevel;
+    }
+
+    public void setAccreditationLevel(String accreditationLevel) {
+        this.accreditationLevel = accreditationLevel;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
