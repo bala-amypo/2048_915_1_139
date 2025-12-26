210752695b42a19d8671ae2f591e6edde985b037
bala-amypo <bala@amypo.in>
2025-12-26 04:58:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseContentTopicController.java
+++ src/main/java/com/example/demo/controller/CourseContentTopicController.java
@@ -1,36 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.CourseContentTopic;
-import com.example.demo.service.CourseContentTopicService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/topics")
 public class CourseContentTopicController {
-
-    @Autowired
-    private CourseContentTopicService topicService;
-
-    @PostMapping
-    public CourseContentTopic create(@RequestBody CourseContentTopic topic) {
-        return topicService.createTopic(topic);
-    }
-
-    @PutMapping("/{id}")
-    public CourseContentTopic update(@PathVariable Long id, @RequestBody CourseContentTopic topic) {
-        return topicService.updateTopic(id, topic);
-    }
-
-    @GetMapping("/{id}")
-    public CourseContentTopic getById(@PathVariable Long id) {
-        return topicService.getTopicById(id);
-    }
-
-    @GetMapping("/course/{courseId}")
-    public List<CourseContentTopic> getByCourse(@PathVariable Long courseId) {
-        return topicService.getTopicsByCourse(courseId);
+    
+    @GetMapping
+    public String getTopics() {
+        return "Topics endpoint";
     }
 }
--- src/main/java/com/example/demo/controller/UniversityController.java
+++ src/main/java/com/example/demo/controller/UniversityController.java
@@ -5,36 +5,29 @@ import com.example.demo.service.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/universities")
 public class UniversityController {
-
+    
     @Autowired
     private UniversityService universityService;
-
+    
     @PostMapping
     public University create(@RequestBody University university) {
         return universityService.createUniversity(university);
     }
-
-    @PutMapping("/{id}")
-    public University update(@PathVariable Long id, @RequestBody University university) {
-        return universityService.updateUniversity(id, university);
-    }
-
+    
     @GetMapping("/{id}")
     public University getById(@PathVariable Long id) {
         return universityService.getUniversityById(id);
     }
-
-    @GetMapping
-    public List<University> getAll() {
-        return universityService.getAllUniversities();
+    
+    @PutMapping("/{id}")
+    public University update(@PathVariable Long id, @RequestBody University university) {
+        return universityService.updateUniversity(id, university);
     }
-
-    @PutMapping("/{id}/deactivate")
+    
+    @DeleteMapping("/{id}")
     public void deactivate(@PathVariable Long id) {
         universityService.deactivateUniversity(id);
     }
